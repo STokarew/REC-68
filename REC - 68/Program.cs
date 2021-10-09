d0ae7a2d6aed3ce8d6f8b7e9990e4e8b471ea432
STokarew <Sseregatokarew@gmail.com>
2021-10-09 14:05:43 +0700

    Добавьте файлы проекта.

---
+++ REC - 68/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace REC_68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите пол: ");
+            string sex = (Console.ReadLine());
+
+            Console.Write("Введите рост: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Возраст: {age} Имя: {name} Пол: {sex} Рост: {height}м  $");
+
+            Console.ReadKey();
+        }
+    }
+}
